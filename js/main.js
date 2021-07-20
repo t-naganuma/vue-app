3e6a6de4cb079f05eba9f1a6d6d3930b9d4a06f1
t-naganuma <takuya.naganuma@gmail.com>
2021-07-20 23:26:30 +0900

    feature: index, add, delete, 分岐

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 (function () {
   'use strict';
   let vm = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
-      
-    }
-  })
+      newItem: "",
+      todos: [
+        // {
+        //   title: "task 1",
+        //   isDone: false,
+        // },
+        // {
+        //   title: "task 2",
+        //   isDone: false,
+        // },
+        // {
+        //   title: "task 3",
+        //   isDone: true,
+        // },
+      ],
+    },
+    methods: {
+      addItem: function (e) {
+        let item = {
+          title: this.newItem,
+          isDone: false
+        }
+        this.todos.push(item);
+        this.newItem = "";
+      },
+      deleteItem: function (index) {
+        if (confirm("are you sure?")) {
+          this.todos.splice(index, 1);
+        }
+      },
+    },
+  });
 })();
