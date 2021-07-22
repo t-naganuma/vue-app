d1df16b2dbb109dc9f4e61557695b1bab4b161aa
t-naganuma <takuya.naganuma@gmail.com>
2021-07-22 09:44:18 +0900

    feat: データ永続化

--- js/main.js
+++ js/main.js
@@ -4,20 +4,18 @@
     el: "#app",
     data: {
       newItem: "",
-      todos: [
-        {
-          title: "task 1",
-          isDone: false,
+      todos: [],
+    },
+    watch: {
+      todos: {
+        handler: function() {
+          localStorage.setItem('todos', JSON.stringify(this.todos));
         },
-        {
-          title: "task 2",
-          isDone: false,
-        },
-        {
-          title: "task 3",
-          isDone: true,
-        },
-      ],
+        deep: true,
+      }
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods: {
       addItem: function (e) {
